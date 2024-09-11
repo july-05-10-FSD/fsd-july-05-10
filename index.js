32821394e6a250ce1fa0287288aaa4c65376d182
krishna7288 <krishna43835@gmail.com>
2024-09-11 16:00:59 +0530

    commit register

--- index.js
+++ index.js
@@ -1,6 +1,99 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://snandhadeveloper592000:nandha56200@cluster0.s6ufz.mongodb.net/server_006?retryWrites=true&w=majority&appName=Cluster0")
+.then(() => {
+    console.log("MongoDb Connected");    
+})
+.catch((e) => {
+    console.log(`connection error: ${e.message}`);    
+});
+
+
+const registerSchema = new mongoose.Schema({
+    userName: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true,
+        trim: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    userType: {
+        type: String,
+        default: "Test"
+    },
+    role: {
+        type: String,
+        enum: ["teacher", "student", "parent"]
+    }
+}, { timestamps: true });
+
+
+const register = mongoose.model("register", registerSchema);
+
+
+
+app.post("/register", async (req, res) => {
+try {
+    let createUser = await register.create(req.body);
+    res.json({
+        createUser,
+        message: "user created"
+    });
+    
+} catch (error) {
+    res.json({
+        Error: error
+    })
+}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let data = {
+    userName: "test user",
+    age: 24,
+    mobileNo: 1234567890
+};
+
+
+app.get("/userdata", (req, res) => {
+    res.json(data)
+});
+
+
+app.post("/createuser", (req, res) => {
+    let data = {
+        ...req.body,
+        email: "test@gmail.com"
+    };
+
+    res.json({
+        data,
+        message: "user created"
+    })
+
+});
+
 
 
 
@@ -10,9 +103,9 @@ app.use("/", (req, res) => {
 });
 
 
-
 const port = 8000;
 
 app.listen(port, () => {
     console.log("Server is running on:", port);
+
 });
